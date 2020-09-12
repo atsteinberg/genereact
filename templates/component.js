197655efb7fdfe6236694a9575893125a9c13930
alexts404 <alexts404dev@gmail.com>
2020-09-12 17:58:42 +0200

    fix: correct a few more typos

--- templates/component.js
+++ templates/component.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './MYCOMPONENT.module.scss'
+import React from 'react';
+import styles from './MYCOMPONENT.module.scss';
 
 function MYCOMPONENT (props) {
   return (
