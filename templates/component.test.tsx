c0c7713e9e87beeb854358a45718949bcb3fc9ea
alexts404 <alexts404dev@gmail.com>
2020-10-09 17:17:28 +0200

    feat: add test template

---
+++ templates/component.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render, screen } from '@testing-library/react';
+
+import MYCOMPONENT from './MYCOMPONENT';
+describe('MYCOMPONENT', () => {
+  it('renders MYCOMPONENT', () => {
+    render(<MYCOMPONENT />);
+    expect(screen.getByText('MYCOMPONENT works!')).not.toBe(null);
+  })
+})
