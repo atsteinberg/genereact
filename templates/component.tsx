73696d68518f0774fbb8d3ea9261c607cbe3bdb2
alexts404 <alexts404dev@gmail.com>
2020-10-13 10:46:48 +0200

    fix: styling and name of globals.scss

--- templates/component.tsx
+++ templates/component.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './MYCOMPONENT.module.scss';
-
 PROPSINTERFACE
-
 const MYCOMPONENT: React.FC<PROPSINTERFACEREF> = (PROPS: PROPSINTERFACEREF) => {
   return <div className={styles.MYCOMPONENT}>MYCOMPONENT works!</div>;
 };
